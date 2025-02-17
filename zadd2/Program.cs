aa18d4569fe9e3953118c8770776904af296cc2b
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-02-17 18:56:53 +0200

    Add files via upload

---
+++ zadd2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadd2
+{
+    internal class Program
+    {
+        static void SelectionSort(Book[] books)
+        {
+            int n = books.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (string.Compare(books[j].Title, books[minIndex].Title, StringComparison.Ordinal) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    Book temp = books[i];
+                    books[i] = books[minIndex];
+                    books[minIndex] = temp;
+                }
+            }
+        }
+
+        static void PrintBooks(Book[] books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Title} - {book.Author}");
+            }
+        }
+
+        static void Main()
+        {
+            Book[] books =
+            {
+            new Book("Война и мир", "Лев Толстой"),
+            new Book("Пипи дългото чорапче", "Астрид Линдгрен"),
+            new Book("Том Сойер", "Марк Твен"),
+            new Book("Под игото", "Иван Вазов"),
+            new Book("Ноев ковчех", "Йордан Радичков")
+        };
+
+            Console.WriteLine("Before Sorting:");
+            PrintBooks(books);
+
+            SelectionSort(books);
+
+            Console.WriteLine("\nAfter Sorting:");
+            PrintBooks(books);
+        }
+    }
+}
---
+++ zadd2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("zadd2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("zadd2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b7b3d4c-637b-4fdc-a2ef-9bd1f0aa0bd7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
