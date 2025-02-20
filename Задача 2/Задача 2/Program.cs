27ae874650c69a54630953d8bf5d745250fbf994
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-02-20 18:45:37 +0200

    Add files via upload

---
+++ Задача 2/Задача 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void InsertionSort(List<Student> students)
+        {
+            for (int i = 1; i < students.Count; i++)
+            {
+                var key = students[i];
+                int j = i - 1;
+
+                while (j >= 0 && (students[j].Grade > key.Grade ||
+                                  (students[j].Grade == key.Grade &&
+                                   string.Compare(students[j].Name, key.Name) > 0)))
+                {
+                    students[j + 1] = students[j];
+                    j--;
+                }
+                students[j + 1] = key;
+            }
+        }
+
+        static void Main()
+        {
+            List<Student> students = new List<Student>
+        {
+            new Student("Максим", 5.32),
+            new Student("Даниел", 5.86),
+            new Student("Живко", 5.57),
+            new Student("Кристиан", 5.65),
+            new Student("Елена", 5.93)
+        };
+
+            InsertionSort(students);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Задача 2/Задача 2/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string name, double grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Grade:F2}";
+        }
+    }
+}
